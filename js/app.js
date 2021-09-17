1cd7e7e1ae28f793d28beb15ba297769fe525228
Mostafa Saleh <mostafasaleh2055@gmail.com>
2021-09-17 07:51:15 +0200

    Created a button that adds new section in navbar.
    I've created a button with an event listener, appended it into the navbar,
     this button listen for a click and creates a new element in the navbar menu
      with an anchor refering to the corresponding section.

--- js/app.js
+++ js/app.js
@@ -37,7 +37,7 @@ x.setAttribute("style", "scroll-behavior: smooth;"); // Making the scroll behavi
 // build the nav
 
 /**
- * Creating 4 Unordered Lists inside the navbar using for loop.
+ * Creating 4 <Unordered Lists> and <anchor> elements inside the navbar using for loop.
 */
 var anc = document.createElement('a');
 uL[0].appendChild(anc);
@@ -49,19 +49,56 @@ for (let i = 1; i <= 3; i++) {
     const sec = document.createElement("ul"); // creating a <ul> element inside the document.
     let anc2 = document.createElement("a"); // creating a <a> element inside the document.
     let secID = ("#section" +(i + 1)); // this is the ID of the section which must be assigned inside the <a> element to enable us to scroll to it when we click on the navbar menu.
+    
     uL[i] = sec; // Assigning i.e. storing the new <ul> element iside the array of Unordered Lists.
+   
     navbar.appendChild(uL[i]); // Appending this <ul> element iside the navbar.
     uL[i].appendChild(anc2); // Appending this <ul> element iside the <ul> element.
+   
     uL[i] = anc2; // Assigning i.e. storing the new <a> element iside the array of Unordered Lists.
+   
     uL[i].textContent = "Section " + (i+1); // Changing the text of this new <ul> element to section and depending on the number of the loop it will be given the name Section (number of the loop).
     uL[i].setAttribute("href", secID); // creating attributes to refer to the corresponding section.
 }
 
 /**
+ * Creating a button which is used to add more sections to the navbar menu.
+ */
+
+const button = document.createElement("button"); // Creating button
+navbar.appendChild(button); // appending it into the navbar
+
+button.textContent = "+";
+
+var i = 3; // This is the index for the sections array, which is used to add new section.
+
+button.addEventListener("click", function () { // an event listener when pressing the button it should create a new section in the navbar menu.
+    i++; 
+   /** Creating new section in the navbar menu */
+    const sec = document.createElement("ul"); // creating a <ul> element inside the document.
+    let anc2 = document.createElement("a"); // creating a <a> element inside the document.
+    let secID = ("#section" +(i + 1)); // this is the ID of the section which must be assigned inside the <a> element to enable us to scroll to it when we click on the navbar menu.
+    
+    uL[i] = sec; // Assigning i.e. storing the new <ul> element iside the array of Unordered Lists.
+   
+    navbar.appendChild(uL[i]); // Appending this <ul> element iside the navbar.
+    uL[i].appendChild(anc2); // Appending this <ul> element iside the <ul> element.
+   
+    uL[i] = anc2; // Assigning i.e. storing the new <a> element iside the array of Unordered Lists.
+   
+    uL[i].textContent = "Section " + (i+1); // Changing the text of this new <ul> element to section and depending on the number of the loop it will be given the name Section (number of the loop).
+    uL[i].setAttribute("href", secID); // creating attributes to refer to the corresponding section.
+
+    alert("Section "+ (i+1) + " has been successfuly added.");
+});
+
+/**
  * Styling the navigation bar and its sections.
 */
+
 var cssStyle = document.createElement("style"); // Creating new style element on the document.
 document.head.appendChild(cssStyle); // appending this style element into the head of the document.
+
 cssStyle.innerHTML = ".navbar__menu {display: flex; flex-wrap:wrap; justify-content: right; background-color: rgb(0, 16, 65);}"; // Creating class to control the nav container as a flexbox.
 
 
@@ -69,6 +106,7 @@ for (let j = 0; j <= uL.length; j++) {
     uL[j].classList.add("menu__link"); // Adding class menu__link in the Stylesheet to the elements of the array (i.e. <ul> elements) using for loop.
     uL[j].setAttribute("style", "font-family: 'Fira Sans', sans-serif; color: white; font-size: 1.15em;"); // setting an attribute that contains the styles for each element in the array.
 }
+
 // Add class 'active' to section when near top of viewport
 
 
