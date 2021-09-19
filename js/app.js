d3985f6cfeb09c6482530254af409315702ceab5
Mostafa Saleh <mostafasaleh2055@gmail.com>
2021-09-19 07:36:52 +0200

    adding the function to highlight the active section in the viewport

--- js/app.js
+++ js/app.js
@@ -156,7 +156,7 @@ for (let j = 0; j < uL.length; j++) {
  var cursorPointer = document.createElement("style");
  document.head.appendChild(cursorPointer);
  cursorPointer.innerHTML = ".navbar__menu ul:hover {cursor: pointer;}";
- 
+
 /**
  * End Main Functions
  * Begin Events
@@ -169,4 +169,27 @@ for (let j = 0; j < uL.length; j++) {
 
 // Set sections as active
 
+
+/**
+ * Adding active class when a section is in the viewport.
+ */
+var secArr = document.querySelectorAll('section'); // creating an array that contains all the section on the document.
+
+window.addEventListener("scroll", function () { //making the window do the function below while scrolling, this fucntion cotinuously checks if the section is in the vieport or not. If yes it adds the active class, if not it remove the active class.
+    secArr.forEach(function (SecInViewport) { // looping over the scetion in the viewport and checking continuously if it's existing or not.
+
+        let bounds = SecInViewport.getBoundingClientRect(); // getting the dimentions of the rectangle of the section being looped and storing it into a variable called bound.
+
+//those are the dimentions that determine if that section is 75% in the viewport or not.
+
+        if (bounds.top <= 350.00 && bounds.top >= -270.00) { // checking if the rectangle is in the viewport or not.
+            if (!SecInViewport.classList.contains("your-active-class")) { //checking if the rectangle doesn't contain the activ class.
+                SecInViewport.setAttribute("class", "your-active-class"); //if the active class doesn't exist and the seciton is in the viewport, it'll add the class as an attribute to the sction in the viewport.
+            }
+        } 
+        else {
+            SecInViewport.removeAttribute("class"); // if the section already has an active class and this section is not in the viewport, it'll remove the class.
+        }
+    });
+});
 
